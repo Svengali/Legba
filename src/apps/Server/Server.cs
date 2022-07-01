1d9d634b7a4e7652362a6e912a64ab1c1abf233c
Marc Hernandez <marcsh@gmail.com>
2022-07-01 16:20:28 -0700

    Update

--- src/apps/Server/Server.cs
+++ src/apps/Server/Server.cs
@@ -1,12 +1,20 @@
 ﻿
 
-
-
-
-
-
-
-
-
-
+/*
 Console.WriteLine( "Hello Server!" );
+
+sv.Server server = new sv.Server( args[0] );
+
+server.startup();
+
+while( server.running )
+{
+	System.Threading.Thread.Sleep( 100 );
+}
+
+server.shutdown();
+*/
+
+
+
+
