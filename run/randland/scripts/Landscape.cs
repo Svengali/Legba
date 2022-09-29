3445003a462455ada1dd1aa1fa5657c232dddf7b
Marc Hernandez <marcsh@gmail.com>
2022-09-28 19:42:03 -0700

    Full updating

--- run/randland/scripts/Landscape.cs
+++ run/randland/scripts/Landscape.cs
@@ -12,13 +12,32 @@ public static class Landscape
 		return 0.0f;
 	}
 
-	static public float ComplexMap( g3.Vector2f pos )
+	static float continentScale = 0.5f;
+	static g3.Vector2f cTrans = new g3.Vector2f( 0.0f, 50.0f );
+
+	static public float ContinentsMap( g3.Vector2f pos )
 	{
-		var vPerlin = rl.Perlin.Fbm( pos, ( p ) => 2, ( p ) => 0.5f, ( p ) => 4 );
+		var vPerlin = 1.0f * rl.Perlin.Fbm( cTrans + pos * continentScale, ( p ) => 2.2f, ( p ) => 0.65f, ( p ) => 3.0f );
 
 		return vPerlin;
 	}
 
+
+
+	static public float ComplexMap( g3.Vector2f pos )
+	{
+		var vPerlin = 0.1f * rl.Perlin.Fbm( pos, ( p ) => 2.2f, ( p ) => 0.65f, ( p ) => 3.0f );
+
+		return vPerlin;
+	}
+
+
+	static public float FullMap( g3.Vector2f pos )
+	{
+		var vPerline = ContinentsMap( pos ) + ComplexMap(pos);
+
+		return vPerline;
+	}
 
 
 
