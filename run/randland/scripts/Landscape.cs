a7cdef3d129bda189c0cbe57a5e320793559439f
Marc Hernandez <marcsh@gmail.com>
2022-10-02 04:37:42 -0700

    Update randland and landscape

--- run/randland/scripts/Landscape.cs
+++ run/randland/scripts/Landscape.cs
@@ -7,20 +7,27 @@ public static class Landscape
 
 
 
-	static public float BasicMap( g3.Vector2f pos )
+	static public float BasicMap( math.Vec2 pos )
 	{
 		return 0.0f;
 	}
 
 	static float continentScale = 0.5f;
-	static g3.Vector2f cTrans  = new g3.Vector2f( 0.0f, 50.0f );
-	static g3.Vector3f cTrans3 = new g3.Vector3f( 0.0f, 50.0f, 0.0f );
+	static math.Vec2 cTrans  = new math.Vec2( 0.0f, 50.0f );
+	static math.Vec3 cTrans3 = new math.Vec3( 0.0f, 50.0f, 0.0f );
 
-	static public float ContinentsMap( g3.Vector2f pos )
+	static public float ContinentsMap( math.Vec2 pos )
 	{
-		var pos3 = new g3.Vector3f( pos.x, pos.y, 0.58f );
+		var pos3 = new math.Vec3( pos.X, pos.Y, 0.58f );
 
-		var vPerlin = 10.0f * rl.Perlin.Fbm( cTrans3 + pos3 * continentScale, ( p ) => 2.2f, ( p ) => 0.65f, ( p ) => 3.0f );
+		var rawPerlin = rl.Perlin.Fbm( cTrans3 + pos3 * continentScale, ( p ) => 2.2f, ( p ) => 0.65f, ( p ) => 3.0f );
+
+		var perlin01 = rawPerlin + 0.44f;
+
+
+		var squishedPerlin = (float)math.fn.SmoothStepSquare( (float)perlin01 );
+
+		var vPerlin = 10.0f * rawPerlin;
 
 		var sign = Math.Sign( vPerlin) >= 0 ? 1.0f : -1.0f;
 
@@ -30,21 +37,27 @@ public static class Landscape
 
 		var v8 = v4 * v4;
 
-		return vPerlin - 1.75f;
+		return perlin01;
 	}
 
 
-	static public float Continents3Map( g3.Vector2f pos )
+	static public float Continents3Map( math.Vec2 pos )
 	{
-		var pos3 = new g3.Vector3f( pos.x, pos.y, 0.59f );
+		var pos3 = new math.Vec3( pos.X, pos.Y, 0.59f );
 
-		var vPerlin = 1.0f * rl.Perlin.Fbm( cTrans3 + pos3 * continentScale, ( p ) => 2.2f, ( p ) => 0.65f, ( p ) => 3.0f );
+		var vPerlin = 1.3f * rl.Perlin.Fbm( cTrans3 + pos3 * continentScale, ( p ) => 2.2f, ( p ) => 0.65f, ( p ) => 3.0f );
+
+		var perlinDomain = vPerlin + 0.0f;
+
+		var perlin01 = math.fn.Clamp( perlinDomain, 0, 1 );
+
+		var squishedPerlin = (float)math.fn.SmoothStepSquare( (float)vPerlin );
 
 		return vPerlin;
 	}
 
 
-	static public float ComplexMap( g3.Vector2f pos )
+	static public float ComplexMap( math.Vec2 pos )
 	{
 		var vPerlin = 0.1f * rl.Perlin.Fbm( pos, ( p ) => 2.2f, ( p ) => 0.65f, ( p ) => 3.0f );
 
@@ -52,7 +65,7 @@ public static class Landscape
 	}
 
 
-	static public float FullMap( g3.Vector2f pos )
+	static public float FullMap( math.Vec2 pos )
 	{
 		var vPerline = ContinentsMap( pos ) + ComplexMap(pos);
 
